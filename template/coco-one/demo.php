01334b20d0a9d357e11a8cd227bb7fa9c2f11a7b
Jan Behrens <dev@jpbehrens.de>
2021-09-03 11:49:16 +0200

    refactored and add Class

---
+++ template/coco-one/demo.php
@@ -0,0 +1,11 @@
+
+<h1>Demo</h1>
+<?php print_r(http_response_code());?>
+<?php var_dump($cocodata)?>
+<? echo $cocomenu->render(PREFIX); ?>
+<span>
+DEINE MUDDA 
+</span>
+
+</body>
+</html>
---
+++ template/coco-one/impressum.php
@@ -0,0 +1,6 @@
+<h1>Impressum</h1>
+<?php print_r(http_response_code());?>
+<?php var_dump($cocodata)?>
+<? echo $cocomenu->render(PREFIX); ?>
+</body>
+</html>
